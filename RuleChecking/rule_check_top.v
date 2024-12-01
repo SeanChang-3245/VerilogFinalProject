f79ae62289638118e42933e3e0acfbf9b1ba8900
SeanChang-3245 <70448313+SeanChang-3245@users.noreply.github.com>
2024-12-01 15:45:07 +0800

    RuleCheck basic structure

---
+++ RuleChecking/rule_check_top.v
@@ -0,0 +1,11 @@
+module RuleCheck_top (
+    input wire clk,
+    input wire rst,
+    input wire [8*18*6-1:0] map,
+
+    output wire rule_valid
+);
+
+    wire [6*18*6-1:0] table_map = map[6*18*6-1:0]; // 檯面
+
+endmodule
