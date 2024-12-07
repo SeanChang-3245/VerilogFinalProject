3e402b6123f9651786c7b6c84990b07b80cddac8
BethTsai <146188828+BethTsai@users.noreply.github.com>
2024-12-07 21:41:56 +0800

    card pixel finished

--- Display/display_top.v
+++ Display/display_top.v
@@ -3,11 +3,11 @@ module Display_top(
     input wire rst,
     input wire interboard_rst,
     input wire en_mouse_display,
-    input wire [6:0] oppo_card_cnt,
-    input wire [6:0] deck_card_cnt,
+    input wire [6:0] oppo_card_cnt,	// display on sevensegment rigth two digits
+    input wire [6:0] deck_card_cnt,	// display on sevensegment left two digits
     input wire [11:0] mouse_pixel,
     input wire [8*18-1:0] sel_card,
-    input wire [8*18*6-1:0] map,
+    input wire [8*18*6-1:0] map,	// 8*18 positions with 54 different types of cards
 
     output wire hsync,
     output wire vsync,
@@ -23,7 +23,9 @@ module Display_top(
     wire clk_25MHz;
     clock_divider m2 (.clk(clk), .clk_div(clk_25MHz));
 
-
+	wire all_rst;
+	assign all_rst = rst | interboard_rst;
+	wire [11:0] card_pixel;
     vga_controller vga_inst(
         .pclk(clk_25MHz),
         .reset(rst),
@@ -34,6 +36,18 @@ module Display_top(
         .v_cnt(v_cnt)
     );
 
+	Draw_card draw_card_inst(
+		.clk(clk),
+		.clk_25MHz(clk_25MHz),
+		.rst(all_rst),
+		.map(map),
+		.h_cnt(h_cnt),
+		.v_cnt(v_cnt),
+		.card_pixel(card_pixel)
+	);
+
+
+	
 
     // use if-else to determine which object should be drawn on the top
     // each object should output a signal to indicate whether is should be drawn at this pixel
@@ -52,8 +66,7 @@ module Display_top(
     //     pixel = bg_pixel
     // end
 
-	// mouse inst ========================================================================
-	// 
+
 
 
 endmodule
---
+++ Display/draw_card.v
@@ -0,0 +1,75 @@
+module Draw_card(
+	input wire clk,
+	input wire clk_25MHz,
+	input wire rst,
+	input [8*18*6-1:0] map,
+	input wire [9:0] h_cnt,
+	input wire [9:0] v_cnt,
+	output reg [11:0] card_pixel
+);
+
+	reg [5:0] card_type;
+	reg [5:0] pixel_x;
+	reg [5:0] pixel_y;
+	mem_pixel_gen mem_pixel_gen_inst(
+		.clk(clk_25MHz),
+		.pixel_x(pixel_x),
+		.pixel_y(pixel_y),
+		.card_type(card_type),
+		.card_pixel(card_pixel)
+	);
+
+	reg [5:0] card_table [0:8*18-1];
+	reg [4:0] x;
+	reg [2:0] y;
+	wire [7:0] position;
+
+
+	integer i;
+	always @(*) begin
+		for(i = 0; i < 144; i = i + 1) begin
+			card_table[i] = map[i*6 -: 6];
+		end
+	end
+
+	// CARD XY GEN
+	assign position = (x*18 + y);
+	always @(*) begin
+		x = h_cnt/32;
+		if(h_cnt >= 32 && h_cnt < 607) begin
+			x = (h_cnt-32)/32;
+		end
+	end
+	always @(*) begin
+		if(v_cnt >= 19 && v_cnt < 349) begin
+			y = (v_cnt-19)/55;
+		end
+		else if(v_cnt >= 360 && v_cnt < 461) begin
+			y = 6 + (v_cnt-360)/55;
+		end 
+		else begin
+			y = 0;
+		end
+	end
+	// CARD TYPE GEN
+	always @(*) begin
+		card_type = card_table[position];
+	end
+	// PIXEL X GEN
+	always @(*) begin
+		pixel_x = 0;
+		if(h_cnt >= 32 && h_cnt < 607) begin
+			pixel_x = h_cnt - 32 - x*32;
+		end
+	end
+	// PIXEL Y GEN
+	always @(*) begin
+		pixel_y = 0;
+		if(v_cnt >= 19 && v_cnt < 349) begin
+			pixel_y = v_cnt - 19 - y*55;
+		end
+		else if(v_cnt >= 360 && v_cnt < 461) begin
+			pixel_y = v_cnt - 360 - (y-6)*55;
+		end
+	end
+endmodule
